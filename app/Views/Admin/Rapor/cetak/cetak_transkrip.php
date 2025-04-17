c5db93e8be4986a4d726ae5e983b31429cd8bca7
lammmzt <alam0098@stmik-wp.ac.id>
2025-04-17 09:28:51 +0700

    update fiture

--- app/Views/Admin/Rapor/cetak/cetak_transkrip.php
+++ app/Views/Admin/Rapor/cetak/cetak_transkrip.php
@@ -66,13 +66,11 @@ p {
 <img src="<?= $imageSrc ?>" alt="Logo" style="width: 100%; text-align: center;">
 <table style="margin-top: 3px;">
     <tr>
-        <td style="width: 25%"></td>
-        <td style="width: 50%; text-align: center;">
+        <td style="text-align: center;">
             <h3 style="font-weight: bold">TRANSKRIP NILAI RAPOR SEMESTER 1 s.d. 5</h3>
             <!-- <h4 style="font-weight: bold">TAHUN AJARAN 2024/2025</h4> -->
             <!-- <p>Nomor : 420 / 128.a</p> -->
         </td>
-        <td style="width: 25%"></td>
     </tr>
 </table>
 <div class="container" style="margin-top: 2px;">
