b6d7f7bb412aaa4c3bf97179eba719eebb37d3cf
lammmzt <alam0098@stmik-wp.ac.id>
2025-05-04 07:15:12 +0700

    update fiture

--- app/Views/Landing/Pengumuman.php
+++ app/Views/Landing/Pengumuman.php
@@ -485,8 +485,9 @@ $('#form_cari_data_rapor').submit(function(e) {
 
                 $('#form_cari').hide();
                 $('#data_siswa').show();
-                var nama_lengkap = response.data.nama_lengkap_data_siswa.charAt(0).toUpperCase() +
-                    response.data.nama_lengkap_data_siswa.slice(1);
+                var data_nama_lengkap = response.data.nama_lengkap_data_siswa;
+                var nama_lengkap = data_nama_lengkap.charAt(0).toUpperCase() +
+                    data_nama_lengkap.slice(1);
                 $('#hasil_nama_lengkap').html(nama_lengkap);
                 $('#hasil_nis').html(response.data.nis_data_siswa);
                 $('#hasil_nisn').html(response.data.nisn_data_siswa);
@@ -494,8 +495,9 @@ $('#form_cari_data_rapor').submit(function(e) {
                     'Laki-laki' :
                     'Perempuan';
                 $('#hasil_jenis_kelamin').html(jenis_kelamin);
-                var tempat_lahir = response.data.tempat_lahir_data_siswa.charAt(0).toUpperCase() +
-                    response.data.tempat_lahir_data_siswa.slice(1);
+                var data_tempat_lahir = response.data.tempat_lahir_data_siswa;
+                var tempat_lahir = data_tempat_lahir.charAt(0).toUpperCase() +
+                    data_tempat_lahir.slice(1);
                 $('#hasil_tempat_lahir').html(tempat_lahir);
                 var tgl = formatDateIndo(response.data.tanggal_lahir_data_siswa);
                 $('#hasil_tanggal_lahir').html(tgl);
