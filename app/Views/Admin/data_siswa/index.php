6a1127171566548c532a8e231eea0553de7722d2
lammmzt <alam0098@stmik-wp.ac.id>
2025-06-12 08:00:16 +0700

    add fiture

--- app/Views/Admin/data_siswa/index.php
+++ app/Views/Admin/data_siswa/index.php
@@ -135,6 +135,7 @@
                                 <th></th>
                                 <th></th>
                                 <th></th>
+                                <th></th>
                             </tr>
                         </thead>
                         <tbody>
